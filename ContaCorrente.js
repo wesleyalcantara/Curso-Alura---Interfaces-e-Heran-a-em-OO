e1e8079708a4858554cdcbea4882588682af9690
wesley <wesleybaadm@gmail.com>
2022-01-06 22:59:14 -0300

    Final da aula 3

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,11 +1,7 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente{
+export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
-
-   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-  
-    
 
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
@@ -21,33 +17,8 @@ export class ContaCorrente{
         return this._saldo;
     }
 
-    constructor(agencia, cliente){
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
+    constructor(cliente, agencia){
+        super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
-    }
-
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
-        this._saldo += valor;           
-    }
-
-    tranferir(valor, conta){
-        
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-        
     }
 }
